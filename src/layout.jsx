481fab7c6a968043e5ca412796f9f03deb8cd053
Henrique Mattos <henrique.mattos@sap.com>
2017-11-05 04:58:41 +0100

    Basic application using webpack, react, bulma, font-awesome, babel

---
+++ src/layout.jsx
@@ -0,0 +1,17 @@
+import React, {Component} from "react";
+import App from "app";
+
+export default class Layout extends App {
+    render() {
+        return (
+            <section className="hero is-fullheight">
+                <div className="hero-body">
+                    <div className="container has-text-centered">
+                        <h1 className="title">Hello World!</h1>
+                        <h2 className="subtitle">Sample Application</h2>
+                    </div>
+                </div>
+            </section>
+        );
+    }
+}
