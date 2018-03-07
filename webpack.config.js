e6e937fdd5c09ec3d4344aae37579c0eb449e4a2
Henrique Mattos <henrique.mattos@sap.com>
2018-03-07 08:07:01 +0100

    Fixed Webpack configuration for build and start scripts

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,6 @@ const path = require("path");
 const webpack = require("webpack");
 const HTMLWebpackPlugin = require("html-webpack-plugin");
 const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
-const ExtractTextPlugin = require("extract-text-webpack-plugin");
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
@@ -12,7 +11,7 @@ module.exports = {
         filename: "index.js"
     },
     module: {
-        loaders: [
+        rules: [
             {
                 test: /\.(jsx|js)$/,
                 exclude: /node_modules/,
@@ -22,17 +21,9 @@ module.exports = {
                 }
             },
             {
-                test: /\.(css|scss|less|sass)$/,
-                include: [
-                    path.resolve(__dirname, "src/css")
-                ],
-                use: ExtractTextPlugin.extract({
-                    fallback: "style-loader",
-                    use: [
-                        "css-loader",
-                        "sass-loader"
-                    ]
-                })
+                test: /\.(css|scss|sass)$/,
+                include: [path.resolve(__dirname, "src/css")],
+                loader: ["style-loader", "css-loader", "sass-loader"]
             },
             {
                 test: /\.(ttf|otf|eot|svg|woff(2)?)(\?[a-z0-9]+)?$/,
@@ -53,24 +44,28 @@ module.exports = {
     },
     performance: {
         hints: "warning",
-        maxAssetSize: 400000,
-        maxEntrypointSize: 400000,
+        maxAssetSize: 600000,
+        maxEntrypointSize: 600000,
         assetFilter: function(assetFilename) {
             return assetFilename.endsWith(".css") || assetFilename.endsWith(".js");
         }
     },
-    devtool: "cheap-module-source-map", // "source-map" | eval" || "cheap-module-source-map" || "cheap-source-map" || "eval-source-map" || "inline-source-map"
     target: "web",
     stats: "detailed",
+    mode: "production",
     plugins: [
+        new webpack.DefinePlugin({
+            "process.env": {
+                "NODE_ENV": JSON.stringify("production")
+            }
+        }),
         new UglifyJsPlugin(),
-        new ExtractTextPlugin("./css/style.css"),
         new HTMLWebpackPlugin({
-            title: "Default Title",
+            title: "Default",
             author: "Visual Works",
-            description: "Description of this project",
-            url: "http://www.domain.com.br",
-            image: "http://www.domain.com.br/img/logo-domain.png",
+            description: "Description",
+            url: "https://www.visualworks.com.br",
+            image: "https://www.visualworks.com.br/img/logo-visualworks.jpg",
             filename: path.resolve(__dirname, "dist/index.html"),
             template: path.resolve(__dirname, "src/index.html")
         }),
@@ -83,16 +78,11 @@ module.exports = {
                 from: path.resolve(__dirname, "src/fonts"),
                 to: path.resolve(__dirname, "dist/fonts")
             }
-        ]),
-        new webpack.DefinePlugin({
-            "process.env": {
-                "NODE_ENV": JSON.stringify("development")
-            }
-        }),
+        ])
     ],
     // advanced
     parallelism: 2,
     profile: true,
-    cache: false,
-    watch: true
+    cache: false
+    // watch: true
 };
